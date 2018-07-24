985a39462fc38f67c1d6fe9552706f4769c89bbf
Michel Desilets <desilets.michel.2007@gmail.com>
2018-07-24 13:33:46 -0400

    Added sidebar to main page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <title>Famille Normandeau-Desilets - Acceuil</title>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/media_query.css" media="screen">
+  <!--  <link rel="stylesheet" href="css/media_query.css" media="screen">-->
   <!--  <link href="https://fonts.googleapis.com/css?family=Alex+Brush|Raleway:400,400i,700,700i" rel="stylesheet">-->
   <?php
   require_once 'classes/database/cl_photosDB.php';
@@ -17,18 +17,14 @@
   $path = $photo->get_F_Path();
   $loc = $path . $file;
   ?>
-
 </head>
 <body>
 <div class="page">
   <!-- ==== START MASTHEAD ==== -->
   <header class="masthead" role="banner">
     <!--    <p class="logo"><a href="/"><img .../></a></p>-->
-    <ul class="social-sites">
-    </ul>
-
-
     <h1>Les Normandeau-Desilets</h1>
+
     <p class="subhead">Une courte histoire de Nous</p>
 
     <nav role="navigation">
@@ -45,63 +41,68 @@
         </li>
       </ul>
     </nav>
-    <img src="<?php echo($loc); ?>" alt="Le Parc Lafontaine" class="home-pic">
   </header>
   <!-- end masthead -->
-  <div class="container">
-    <!-- ==== START MAIN ==== -->
-    <main role="main">
-      <section class="post">
-        <!--        <img ... class="post-photo-full"/>-->
-        <div class="post-blurb">
-          <!--          <p>It is hard to believe ...</p>-->
-        </div>
-        <footer class="footer">
-          <!--          ... [blog post snippet footer] ...-->
-        </footer>
-      </section>
-      <section class="post">
-        <!--        <h1>The City Named After Queen Victoria</h1>-->
-        <!--        <img ... class="post-photo"/>-->
-        <div class="post-blurb">
-          <!--          <p>An hour and a half aboard ...</p>-->
-        </div>
-        <footer class="footer">
-          <!--          ... [blog post snippet footer] ...-->
-        </footer>
-      </section>
-      <nav role="navigation">
-        <ol class="pagination">
-          <!--          ... [links list items] ...-->
-        </ol>
-      </nav>
-    </main>
-    <!-- end main -->
-    <!-- ==== START SIDEBAR ==== -->
-    <div class="sidebar">
-      <article class="about">
-        <!--        <h2>About Me</h2>-->
-        <!--        ...-->
-      </article>
-      <div class="mod">
-        <!--<h2>My Travels</h2>
-        ... [map image] ...-->
-      </div>
-      <aside class="mod">
-        <!--<h2>Popular Posts</h2>
-        <ul class="links">
-          ... [links list items] ...
-        </ul>-->
-      </aside>
-      <aside class="mod">
-        <!--        <h2>Recently Shared</h2>-->
-        <ul class="links">
-          <!--          ... [links list items] ...-->
-        </ul>
-      </aside>
+  <!-- ==== START SIDEBAR ==== -->
+  <div class="sidebar">
+    <article class="about">
+      <img src="img/archives/bernard-normandeau/preview/Chantal_Normandeau_1973-CEGEP.jpg" alt="Chantal Normandeau"
+           class="myself">
+      <p>En construisant ce site, j'ai voulu, tenté à tout le moins, conserver l'histoire de ma famille et
+        un peu de celles avec qui j'ai partagé ma vie.</p>
+      <!--        <h2>About Me</h2>-->
+      <!--        ...-->
+    </article>
+    <div class="mod">
+      <!--<h2>My Travels</h2>
+      ... [map image] ...-->
     </div>
-    <!-- end sidebar -->
+    <aside class="mod">
+      <!--<h2>Popular Posts</h2>
+      <ul class="links">
+        ... [links list items] ...
+      </ul>-->
+    </aside>
+    <aside class="mod">
+      <!--        <h2>Recently Shared</h2>-->
+      <ul class="links">
+        <!--          ... [links list items] ...-->
+      </ul>
+    </aside>
   </div>
+  <!-- end sidebar -->
+</div>
+<div class="container">
+  <!-- ==== START MAIN ==== -->
+  <main role="main">
+    <section class="post">
+      <img src="<?php echo($loc); ?>" alt="Le Parc Lafontaine">
+      <!--        <img ... class="post-photo-full"/>-->
+      <div class="post-blurb">
+        <!--          <p>It is hard to believe ...</p>-->
+      </div>
+      <footer class="footer">
+        <!--          ... [blog post snippet footer] ...-->
+      </footer>
+    </section>
+    <section class="post">
+      <!--        <h1>The City Named After Queen Victoria</h1>-->
+      <!--        <img ... class="post-photo"/>-->
+      <div class="post-blurb">
+        <!--          <p>An hour and a half aboard ...</p>-->
+      </div>
+      <footer class="footer">
+        <!--          ... [blog post snippet footer] ...-->
+      </footer>
+    </section>
+    <nav role="navigation">
+      <ol class="pagination">
+        <!--          ... [links list items] ...-->
+      </ol>
+    </nav>
+  </main>
+  <!-- end main -->
+
   <!-- end container -->
   <!-- ==== START PAGE FOOTER ==== -->
   <footer role="contentinfo" class="footer">
