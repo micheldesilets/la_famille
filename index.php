9811ce9c31296fe40247026ad92fcc5669026baf
Michel Desilets <desilets.michel.2007@gmail.com>
2018-07-27 11:32:48 -0400

    Added my picture

--- index.php
+++ index.php
@@ -58,9 +58,9 @@
     <article class="about">
       <img src="<?php echo($locSb); ?>" alt="Chantal Normandeau"
            class="myself">
-      <p>En construisant ce site, j'ai voulu, tenté à tout le moins, conserver l'histoire de ma famille et
-        un peu celle avec qui j'ai partagé ma vie.</p>
-      <img src="img/Signature.jpg" alt="" class="signature">
+      <p>En construisant ce site, j'ai voulu, tenté à tout le moins, conserver l'histoire de ma famille et un peu celle
+        de ceux avec qui j'ai partagé ma vie.</p>
+      <img src="img/home/Signature.jpg" alt="" class="signature">
       <!--        <h2>About Me</h2>-->
       <!--        ...-->
     </article>
